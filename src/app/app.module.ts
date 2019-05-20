aaf036166c51be7ab5b53dc861319e52dae1b0cf
Matthew McIntyre <mattymac2@gmail.com>
2019-05-20 22:39:36 +0100

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { PricingComponent } from './pricing/pricing.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./home/about/about.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FindJobsComponent } from "./find-jobs/find-jobs.component";
+import { LoginComponent } from "./login/login.component";
+import { BookDemoComponent } from "./book-demo/book-demo.component";
+import { LandingComponent } from "./home/landing/landing.component";
 
 @NgModule({
   declarations: [
@@ -16,13 +20,14 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     AboutComponent,
     PricingComponent,
-    ContactComponent
+    ContactComponent,
+    FindJobsComponent,
+    LoginComponent,
+    BookDemoComponent,
+    LandingComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
