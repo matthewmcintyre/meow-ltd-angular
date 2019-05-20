9a394535d85fb520af94d832d2bda2458a297264
Matthew McIntyre <mattymac2@gmail.com>
2019-05-20 22:39:02 +0100

    configure app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { FindJobsComponent } from "./find-jobs/find-jobs.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "contact", component: ContactComponent }
+  { path: "contact", component: ContactComponent },
+  { path: "pricing", component: PricingComponent },
+  { path: "find-jobs", component: FindJobsComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
